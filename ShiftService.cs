56cc23f4993560602bf0632ea004e358ffce19e5
Hartono Halim <gopher@equalbyte.com>
2017-04-20 13:37:03 -0500

    Update to Shift 1.0.7.0

--- ShiftService.cs
+++ ShiftService.cs
@@ -25,27 +25,32 @@ namespace Shift.WinService
             if (jobServer == null)
             {
                 var config = new Shift.ServerConfig();
-                config.AssemblyFolder = ConfigurationManager.AppSettings["AssemblyFolder"];
-                //config.AssemblyListPath = ConfigurationManager.AppSettings["AssemblyListPath"];
                 config.MaxRunnableJobs = Convert.ToInt32(ConfigurationManager.AppSettings["MaxRunableJobs"]);
                 config.ProcessID = ConfigurationManager.AppSettings["ShiftPID"];
                 config.DBConnectionString = ConfigurationManager.ConnectionStrings["ShiftDBConnection"].ConnectionString;
+                config.Workers = Convert.ToInt32(ConfigurationManager.AppSettings["ShiftWorkers"]);
 
                 config.StorageMode = ConfigurationManager.AppSettings["StorageMode"];
                 var progressDBInterval = ConfigurationManager.AppSettings["ProgressDBInterval"];
                 if (!string.IsNullOrWhiteSpace(progressDBInterval))
                     config.ProgressDBInterval = TimeSpan.Parse(progressDBInterval); //Interval when progress is updated in main DB
 
-                config.ServerTimerInterval = Convert.ToInt32(ConfigurationManager.AppSettings["TimerInterval"]); //optional: default every 5 sec for getting jobs ready to run and run them
-                config.ServerTimerInterval2 = Convert.ToInt32(ConfigurationManager.AppSettings["CleanUpTimerInterval"]); //optional: default every 10 sec for server CleanUp()
-
                 var autoDeletePeriod = ConfigurationManager.AppSettings["AutoDeletePeriod"];
                 config.AutoDeletePeriod = string.IsNullOrWhiteSpace(autoDeletePeriod) ? null : (int?)Convert.ToInt32(autoDeletePeriod);
 
+                config.AssemblyFolder = ConfigurationManager.AppSettings["AssemblyFolder"];
+                //config.AssemblyListPath = ConfigurationManager.AppSettings["AssemblyListPath"];
+
+                config.ForceStopServer = Convert.ToBoolean(ConfigurationManager.AppSettings["ForceStopServer"]); //Set to true to allow windows service to shut down after a set delay in StopServerDelay
+                config.StopServerDelay = Convert.ToInt32(ConfigurationManager.AppSettings["StopServerDelay"]);
+
+                config.ServerTimerInterval = Convert.ToInt32(ConfigurationManager.AppSettings["TimerInterval"]); //optional: default every 5 sec for getting jobs ready to run and run them
+                config.ServerTimerInterval2 = Convert.ToInt32(ConfigurationManager.AppSettings["CleanUpTimerInterval"]); //optional: default every 10 sec for server CleanUp()
                 //config.AutoDeleteStatus = new List<JobStatus?> { JobStatus.Completed, JobStatus.Error }; //Auto delete only the jobs that had Stopped or with Error
                 //config.UseCache = Convert.ToBoolean(ConfigurationManager.AppSettings["UseCache"]);
                 //config.CacheConfigurationString = ConfigurationManager.AppSettings["RedisConfiguration"];
                 //config.EncryptionKey = ConfigurationManager.AppSettings["ShiftEncryptionParametersKey"];
+                config.PollingOnce = Convert.ToBoolean(ConfigurationManager.AppSettings["PollingOnce"]);
 
                 jobServer = new Shift.JobServer(config);
             }
